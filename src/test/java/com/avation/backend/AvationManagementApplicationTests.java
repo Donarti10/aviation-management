354613ff6bd3bc6be0f2cbfc9ae23bd5ea28994d
Donarti10 <123815608+Donarti10@users.noreply.github.com>
2023-12-11 18:49:51 +0100

    added project in github

---
+++ src/test/java/com/avation/backend/AvationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avation.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
